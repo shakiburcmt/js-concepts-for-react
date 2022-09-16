cbca00b092d9b2415fbbed28cdcd0dad222ec36b
shakiburcmt <shakibur.cmt@gmail.com>
2022-09-16 16:42:16 +0600

    update

---
+++ objShortcut.js
@@ -0,0 +1,11 @@
+const name = 'Shakib';
+const wifeName = 'Mariam';
+const obj = {
+    name:name,
+    wifeName:wifeName
+}
+console.log(obj)
+// now time to declare shortcut
+const object = { name, wifeName };
+// ekhane name dile js automatic dhore ney je name ekta variable o ache
+console.log(object)
